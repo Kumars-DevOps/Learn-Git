34075b9a8ff24c9caa270c429f11c4c617e0f30e
Kumars-DevOps <124190235+Kumars-DevOps@users.noreply.github.com>
2025-12-27 18:43:48 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,20 +1,27 @@
-resource "aws_s3_bucket" "anilbucket" {
-    bucket = "anilbucket08800123"
+#resource "aws_s3_bucket" "anilbucket" {
+ #   bucket = "anilbucket08800123"
+#
+ #   tags = {
+#      name = "anilbucket"
+ #       environment = "dev"
+  #  }
+#}
 
+#resource "local_file" "myfirstfile" {
+ #   filename = "myfirstfile.txt"
+  #  content  = "this is my first file"
+   # }
+#
+#resource " aws_vpc" "anilvpc" {#    cidr_block = "10.0.0.0/16"
+   # tags = {
+  #      Name = "anilvpc"
+ #   }
+#}
+
+resource "aws_s3_bucket" "anilbucket" {
+    bucket = "anilbucket233333m"
     tags = {
-        name = "anilbucket"
-        environment = "dev"
+        Name = "anilbucket"
+        Environment = "prod"
     }
 }
-
-resource "local_file" "myfirstfile" {
-    filename = "myfirstfile.txt"
-    content  = "this is my first file"
-    }
-
-#resource " aws_vpc" "anilvpc" {
-#    cidr_block = "10.0.0.0/16"
-#    tags = {
-#        Name = "anilvpc"
-#    }
-#}
